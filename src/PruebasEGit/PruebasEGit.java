5da22af14fa7b8b9e8ad040f7a5da7944676c3dc
Nicolás Alonso <nicoalonsof@gmail.com>
2025-03-07 13:04:56 +0100

    Primer commit desde rama1

--- src/PruebasEGit/PruebasEGit.java
+++ src/PruebasEGit/PruebasEGit.java
@@ -5,7 +5,7 @@ public class PruebasEGit {
 	public static void main(String[] args) {
 		System.out.println("pruebas EGit");
 		System.out.println("Primer Cambio");
-		
+		System.out.println("Cambio desde rama1");
 
 	}
 
