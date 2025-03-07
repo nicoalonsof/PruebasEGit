f05778b32bbe20e9ebc88eed4e90b2d45ad52086
Nicolás Alonso <nicoalonsof@gmail.com>
2025-03-07 12:24:31 +0100

    Segundo commit

--- src/PruebasEGit/PruebasEGit.java
+++ src/PruebasEGit/PruebasEGit.java
@@ -4,18 +4,7 @@ public class PruebasEGit {
 
 	public static void main(String[] args) {
 		System.out.println("pruebas EGit");
-		
-		//quiero realizar una codigo que funcione como una calculadora
-		
-		int a = 5;
-		int b = 3;
-		
-		System.out.println("La suma de " + a + " + " + b + " es " + (a+b));
-		System.out.println("La resta de " + a + " - " + b + " es " + (a-b));
-		System.out.println("La multiplicacion de " + a + " * " + b + " es " + (a*b));
-		System.out.println("La division de " + a + " / " + b + " es " + (a/b));
-		System.out.println("El resto de la division de " + a + " % " + b + " es " + (a%b));
-		
+		System.out.println("Primer Cambio");
 		
 
 	}
